eaa6bd45827b37b62639dfc378d1b5c9e880fe16
incoverlet <incoverlet@gmail.com>
2022-02-25 14:27:18 +0900

    fbv로 포스트 목록 blog에 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+    # 최신글 맨위에 올리는 법
+    posts = Post.objects.all().order_by('-pk')
+
+    # 데이터베이스에 쿼리를 날려 원하는 레코드 가져옴
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts' : posts,
+        }
+    )
